bfd54aefd9832ab7aad149f02b39ea6b8d6108fe
mlmoreto <marialauramoreto@hotmail.com>
2021-01-12 15:25:23 -0300

    Add ProductsList and ProductCard

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,13 +8,13 @@ function Footer(){
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="" target="_new">
+                <a href="/" target="_new">
                     <YouTubeIcon />
                 </a>
-                <a href="" target="_new">
+                <a href="/" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="" target="_new">
+                <a href="/" target="_new">
                     <InstagramIcon />
                 </a>
             </div>
--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders(){
+
+    const[products, setProducts] =useState<Product[]>([]);
+    //console.log(products)
+
+    useEffect(() => {
+        //console.log('componente orders iniciou');
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeaders />
+            <ProductsList products={products}/>
         </div>
     )
 }
